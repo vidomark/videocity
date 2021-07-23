b6370707b13542f459d979985ae9c055271030e8
Vidó Márk <vidomark42@gmail.com>
2021-07-23 14:42:03 +0200

    Services - initial service structure upload

---
+++ sharing-service/src/main/java/com/codecool/sharing_service/SharingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.sharing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharingServiceApplication.class, args);
+    }
+
+}
---
+++ sharing-service/src/test/java/com/codecool/sharing_service/SharingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.sharing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
